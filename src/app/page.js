6b2e712a62413f652bece21e4cbbee4bfab8033d
kennedyifeadi <kennedyifeadi@gmail.com>
2025-03-12 16:59:13 +0100

    Refactor page.js to update dashboard values dynamically

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import { Navbar } from "./components/NavBar";
 import { Sidebar } from "./components/Sidebar";
@@ -11,19 +13,42 @@ import { FaTemperatureFull } from "react-icons/fa6";
 import "./globals.css";
 import { AiInference } from "./components/Aisurmmary";
 import { FishFarmUpdate } from "./components/fishFarmUpdate";
+import {db, get, onValue, ref} from "./config/firebase"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tds, setTds] = useState(0)
+  const [turbidity, setTurbidity] = useState(0)
+  const [ammonia, setAmmonia] = useState(0)
+  const [ph, setPh] = useState(0)
+
+  const updateDashboard = () => {
+    onValue(ref(db, "Data"), (snapshot) => {
+      const Data = snapshot.val();
+      setTds(Data?.tds);
+      setTurbidity(Data?.turbidity);
+      setPh(Data?.ph);
+      setAmmonia(Data?.ammonia);
+    });
+   
+  };
+
+
+  useEffect(()=>{
+    updateDashboard();
+  }, [])
+
   return (
     <div className="w-[100%] md:h-[100dvh] overflow-x-hidden mb-3 md:mb-0 h-max flex flex-col py-2 relative">
       <div className=" flex md:flex-row flex-col px-4 py-2 w-full justify-between h-[100dvh] md:h-[90%]">
         <Sidebar />
         <div className="flex w-[100%] md:w-[45%] md:h-max md:ml-4">
           <div className="flex flex-wrap w-[100dvw] items-center h-max  gap-4">
-            <DashboardCard title="Ammonia (NH3)" value="50 PPM" status="Normal" icon={<FiBook />} text="Optimal nitrate levels for fish health" color="dark" />
-            <DashboardCard title="Turbidity" value="25 PPM" status="Soft" icon={<CiDroplet />} text="Soft water, ideal for specific fish species" color="dark" />
-            <DashboardCard title="TDS" value="80 PPM" status="Medium" icon={<CiCloudDrizzle />} text="Balanced buffering capacity to maintain" color="dark" />
+            <DashboardCard title="Ammonia (NH3)" value={ammonia} status="Normal" icon={<FiBook />} text="Optimal nitrate levels for fish health" color="dark" />
+            <DashboardCard title="Turbidity" value={`${turbidity} PPM`} status="Soft" icon={<CiDroplet />} text="Soft water, ideal for specific fish species" color="dark" />
+            <DashboardCard title="TDS" value={tds} status="Medium" icon={<CiCloudDrizzle />} text="Balanced buffering capacity to maintain" color="dark" />
             <DashboardCard title="Temperature" value="80°C" status="Medium" icon={<FaTemperatureFull />} text="Ideal temperature for healthy fish growth" color="dark" />
-            <DashboardCard title="PH" value="25 PPM" status="Soft" icon={<CiDroplet />} text="Soft water, ideal for specific fish species" color="dark" />
+            <DashboardCard title="PH" value={ph} status="Soft" icon={<CiDroplet />} text="Soft water, ideal for specific fish species" color="dark" />
 
           </div>
         </div>
@@ -39,3 +64,7 @@ export default function Home() {
     </div>
   );
 }
+
+
+// api key = AIzaSyCGfvmDY4Fzjjh4gb3R7CL5BS6QadPuC8Q
+// url = https://blue-wave-cf3f2-default-rtdb.firebaseio.com/
